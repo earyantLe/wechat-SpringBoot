df632eb4fbd6834c278882b6622b01d723e8ccf8
lirui940403@outlook.com <06lirx>
2017-04-26 22:08:08 +0800

    初始化项目

---
+++ src/main/java/com/earyant/wechat/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.earyant.wechat;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WechatApplication.class);
+	}
+
+}
